5fe17ea7083bc6b67b3869287562a4dab394727b
hegej <hege.jacobsen12@gmail.com>
2023-10-17 20:04:57 +0200

    Add project files.

---
+++ Controllers/QuestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AncientRelicQuest.Services;
+
+namespace AncientRelicQuest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestController : ControllerBase
+    {
+        private readonly QuestTask _questTask;
+
+        public QuestController(QuestTask questTask)
+        {
+            _questTask = questTask;
+        }
+
+        [HttpGet("GetRiddle")]
+        public ActionResult<Riddle> GetRiddle()
+        {
+            return Ok(_questTask.GetRandomRiddle());
+        }
+
+        [HttpPost("EvaluateAnswer")]
+        public ActionResult<bool> EvaluateAnswer(int riddleId, string answer)
+        {
+            return Ok(_questTask.EvaluateAnswer(riddleId, answer));
+        }
+    }
+}
+
---
+++ Services/QuestTask.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace AncientRelicQuest.Services
+{
+    public class Riddle
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+
+    public class QuestTask
+    {
+        private List<Riddle> riddles;
+
+        public QuestTask()
+        {
+            var json = File.ReadAllText("JsonData/ChatMessages.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            riddles = JsonConvert.DeserializeObject<List<Riddle>>(data.riddles.ToString());
+        }
+
+        public Riddle GetRandomRiddle()
+        {
+            return riddles[new Random().Next(0, riddles.Count)];
+        }
+
+        public bool EvaluateAnswer(int riddleId, string userAnswer)
+        {
+            var riddle = riddles.FirstOrDefault(r => r.Id == riddleId);
+            if (riddle != null)
+            {
+                return string.Equals(riddle.Answer, userAnswer, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+
+    }
+}
